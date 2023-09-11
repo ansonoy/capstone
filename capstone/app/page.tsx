b5509a2d4528b04ec785790f96f15070a8f222dc
ansonoy <ansonoy00@gmail.com>
2023-09-11 14:19:47 -0600

    font size change

--- capstone/app/page.tsx
+++ capstone/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main>
-      <div className={`flex h-screen items-center justify-center`}>Whispering Dutchman Distillery</div>
+      <div className={`flex h-screen items-center justify-center text-7xl font-semibold`}>Whispering Dutchman Distillery</div>
     </main>
   )
 }
