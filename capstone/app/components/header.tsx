2f7ef0a791c443eda898d1b486d9bf1fa7f2c233
ansonoy <ansonoy00@gmail.com>
2023-09-18 12:33:57 -0600

    ADDED HEADER

---
+++ capstone/app/components/header.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from "react"
+import Link from "next/link"
+import { motion } from "framer-motion"
+
+export default function Header() {
+  return (
+    <nav
+      className={`px-auto py-4 fixed top-0 left-0 right-0 backdrop-blur-lg z-[999]`}
+    >
+      <ul className="flex items-center justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 text-xs sm:text-base md:text-lg font-semibold">
+        <motion.li whileHover={{ scale: 1.1 }}>Header</motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>Header</motion.li>
+      </ul>
+    </nav>
+  )
+}
--- capstone/app/page.tsx
+++ capstone/app/page.tsx
@@ -3,8 +3,12 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main>
-      <div className={`flex h-screen items-center justify-center text-7xl font-semibold`}>Whispering Dutchman Distillery</div>
-      <h1>Jacob</h1>
+      <div className={`flex h-screen items-center justify-center`}>
+        <h1 className={`text-7xl font-semibold`}>
+          Whispering Dutchman Distillery
+          </h1>
+      </div>
+  
     </main>
   )
 }
