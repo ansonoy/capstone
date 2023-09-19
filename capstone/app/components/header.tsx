26842c8a24edceaf0c2cfaf819727ed0e89e9e4a
ansonoy <ansonoy00@gmail.com>
2023-09-19 08:26:10 -0600

    idk

--- capstone/app/components/header.tsx
+++ capstone/app/components/header.tsx
@@ -9,8 +9,12 @@ export default function Header() {
       className={`px-auto py-4 fixed top-0 left-0 right-0 backdrop-blur-lg z-[999]`}
     >
       <ul className="flex items-center justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 text-xs sm:text-base md:text-lg font-semibold">
-        <motion.li whileHover={{ scale: 1.1 }}>Header</motion.li>
-        <motion.li whileHover={{ scale: 1.1 }}>Header</motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">Home</Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">Header</Link>
+        </motion.li>
       </ul>
     </nav>
   )
