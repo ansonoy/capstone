fc72d571d4b06cc53b30fd71514a280169f3304e
ansonoy <ansonoy00@gmail.com>
2023-09-27 14:24:04 -0600

    finished header?

--- capstone/app/page.tsx
+++ capstone/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main>
-      <div className={`flex h-screen items-center justify-center bg-gray-900`}>
+      <div className={`flex h-screen items-center justify-center bg-gray-700`}>
         <h1 className={`text-7xl font-semibold`}>
           Whispering Dutchman Distillery
           </h1>
