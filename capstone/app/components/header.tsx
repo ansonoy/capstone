818ce1cc66c7edfe8e2086ab6867b6a9870d4e1b
ansonoy <ansonoy00@gmail.com>
2023-09-27 14:12:55 -0600

    implement all pages

--- capstone/app/components/header.tsx
+++ capstone/app/components/header.tsx
@@ -2,6 +2,7 @@
 import React from "react"
 import Link from "next/link"
 import { motion } from "framer-motion"
+import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai"
 
 export default function Header() {
   return (
@@ -10,10 +11,32 @@ export default function Header() {
     >
       <ul className="flex items-center justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 text-xs sm:text-base md:text-lg font-semibold">
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">Home</Link>
+          <Link href="/">The Story</Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">Header</Link>
+          <Link href="/">Location</Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">Barrel Fund</Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">Shop</Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">Recipes</Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">Contact</Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">
+            <AiOutlineShoppingCart size="24" />
+          </Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">
+            <AiOutlineUser  size="24"/>
+          </Link>
         </motion.li>
       </ul>
     </nav>
