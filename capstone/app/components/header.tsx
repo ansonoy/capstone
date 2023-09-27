13408eb8c11380c810067738dafeaa7e3a606353
ansonoy <ansonoy00@gmail.com>
2023-09-27 14:23:28 -0600

    finished headerish

--- capstone/app/components/header.tsx
+++ capstone/app/components/header.tsx
@@ -1,6 +1,8 @@
 "use client"
 import React from "react"
 import Link from "next/link"
+import Image from 'next/image'
+import WDD from '@/public/WDD.png'
 import { motion } from "framer-motion"
 import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai"
 
@@ -11,31 +13,41 @@ export default function Header() {
     >
       <ul className="flex items-center justify-center space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 text-xs sm:text-base md:text-lg font-semibold">
         <motion.li whileHover={{ scale: 1.1 }}>
+          <Link href="/">
+            <Image
+              src={WDD}
+              width={36}
+              height={36}
+              alt="/images/WDD.png"
+            ></Image>
+          </Link>
+        </motion.li>
+        <motion.li whileHover={{ scale: 1.1 }}>
           <Link href="/">The Story</Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
           <Link href="/">Location</Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">Barrel Fund</Link>
+          <Link href="/barrel_fund">Barrel Fund</Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">Shop</Link>
+          <Link href="/shop">Shop</Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">Recipes</Link>
+          <Link href="/recipes">Recipes</Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">Contact</Link>
+          <Link href="/contact">Contact</Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">
+          <Link href="/cart">
             <AiOutlineShoppingCart size="24" />
           </Link>
         </motion.li>
         <motion.li whileHover={{ scale: 1.1 }}>
-          <Link href="/">
-            <AiOutlineUser  size="24"/>
+          <Link href="/account">
+            <AiOutlineUser size="24" />
           </Link>
         </motion.li>
       </ul>
--- capstone/app/page.tsx
+++ capstone/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main>
-      <div className={`flex h-screen items-center justify-center`}>
+      <div className={`flex h-screen items-center justify-center bg-gray-900`}>
         <h1 className={`text-7xl font-semibold`}>
           Whispering Dutchman Distillery
           </h1>
