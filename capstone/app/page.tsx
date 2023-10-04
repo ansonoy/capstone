ff543f6c47a44d218479e170c8e1a1d6355f4dfe
ansonoy <ansonoy00@gmail.com>
2023-10-04 14:18:22 -0600

    added psql url

--- capstone/app/page.tsx
+++ capstone/app/page.tsx
@@ -2,13 +2,24 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <main>
-      <div className={`flex h-screen items-center justify-center bg-gray-700`}>
-        <h1 className={`text-7xl font-semibold`}>
-          Whispering Dutchman Distillery
-          </h1>
+    <main className={`bg-gray-700`}>
+      <div className={`flex flex-row h-screen items-center justify-center`}>
+        <h1
+          className={`text-5xl sm:text-7xl font-semibold flex flex-col bg-red-500`}
+        >
+          <p>Whispering</p> <p>Dutchman</p> <p>Distillery</p>
+        </h1>
       </div>
-  
+      <div>
+        <h1 className={`flex justify-center text-4xl font-semibold h-[30rem]`}>
+          The Story
+        </h1>
+      </div>
+      <div>
+        <h1 className={`flex justify-center text-4xl font-semibold h-[30rem]`}>
+          Location
+        </h1>
+      </div>
     </main>
   )
 }
