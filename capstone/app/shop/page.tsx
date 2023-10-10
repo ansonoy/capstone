55d3543da7a42b67d387e980a180cdec2fc17bab
Anson <ansonoy00@gmail.com>
2023-10-10 16:08:19 -0600

    prisma is done

--- capstone/app/shop/page.tsx
+++ capstone/app/shop/page.tsx
@@ -2,17 +2,8 @@ import Product from "../components/product"
 
 export default function Shop() {
   return (
-      <div className={`bg-white h-screen flex items-center`}>
-          
-          <Product
-              price={30}
-              name="Bottle'O'Gin"
-              type="Gin"
-            percentage={1000}
-          />
-        
-             
-          
+    <div className={`bg-white h-screen flex items-center`}>
+      <Product price={30} name="Bottle'O'Gin" type="Gin" percentage={1000} />
     </div>
   )
 }
