c785b2b459d5be570fb8ced38e6d01a9434144c5
JacobGobes <jacob_goble@hotmail.com>
2023-10-18 13:10:43 -0600

    a\created recipe component bug on the image

--- capstone/app/recipes/page.tsx
+++ capstone/app/recipes/page.tsx
@@ -1,3 +1,26 @@
-export default function Recipes() {
-    
+import React from "react"
+import Recipe from "../components/recipe"
+import ginImage from "@/public/signature_gin.png"
+import Image from "next/image"
+
+export default function Recipe() {
+  return (
+    <div className={`bg-grey h-screen flex items-center `}>
+      <Recipe
+              image={
+                  <div>
+                    <Image
+                src={ginImage}
+                width={300}
+                height={700}
+                alt="/image/signature_gin.png"
+                ></Image>    
+                  </div>
+
+        }
+        name="Bottle'O'Gin"
+        description="recipe"
+      />
+    </div>
+  )
 }
