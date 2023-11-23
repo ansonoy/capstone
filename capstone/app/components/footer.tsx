90defc6e99f485500f500f3aed2c143c6a661e71
Anson <ansonoy00@gmail.com>
2023-11-23 11:01:47 -0700

    test again

--- capstone/app/components/footer.tsx
+++ capstone/app/components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 export default function Footer() {
-  const hiddenFooter = ["/admin"]
+  const hiddenFooter = ["/admin", null]
   const isFooterHidden = hiddenFooter.includes(usePathname())
   return (
     <>
