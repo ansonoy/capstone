1505efcaeb38cc3ed76353ac72cc16252257054c
Anson <ansonoy00@gmail.com>
2023-11-30 13:22:08 -0700

    updated shop page

--- capstone/app/components/products-card.tsx
+++ capstone/app/components/products-card.tsx
@@ -22,7 +22,7 @@ export default function Products({ product }: ProductsProps) {
       <ul className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-3 text-lg text-gray-800">
         {product.map((product) => (
           <div
-            className="pt-4 p-2 pb-5 flex flex-col max-w-[30rems] bg-gradient-to-l from-black via-gray-900 to-black text-white rounded hover:scale-[1.05] transition-all group cursor-pointer"
+            className="pt-4 p-2 pb-5 flex flex-col max-w-[40rems] bg-gradient-to-l from-black via-gray-900 to-black text-white rounded hover:scale-[1.05] transition-all group cursor-pointer"
             onClick={() => {
               router.push(`/shop/${product.id}`)
             }}
@@ -30,8 +30,8 @@ export default function Products({ product }: ProductsProps) {
             <div className="flex justify-center">
               <Image
               src={product.imageurl}
-              width={300}
-              height={300}
+              width={400}
+              height={400}
               alt={product.name}
             />
             </div>
--- capstone/app/shop/page.tsx
+++ capstone/app/shop/page.tsx
@@ -20,7 +20,7 @@ export default async function Shop() {
         header="Shop"
         text="Explore our exclusive shop page, where you can elevate your collection with the crisp allure of our signature vodka and the botanical symphony of our artisanal gin; each bottle is a celebration of quality and flavor, embodying the essence of extraordinary craftsmanship."
       />
-      <div className="w-full flex justify-center py-8 px-4">
+      <div className="w-full flex justify-center py-8 px-2 sm:px-4 lg:px-8">
         <Products product={products} />
       </div>
     </main>
