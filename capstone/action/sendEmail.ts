4c0b6a153552157ef9692ca4c44f70371a3e5eb3
Anson <ansonoy00@gmail.com>
2023-11-29 22:39:36 -0700

    fixed contact

--- capstone/action/sendEmail.ts
+++ capstone/action/sendEmail.ts
@@ -24,7 +24,7 @@ export const sendEmail = async (FormData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "anson.ouyang@edu.sait.ca",
+      to: "ansonoy00@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
       text: message as string
