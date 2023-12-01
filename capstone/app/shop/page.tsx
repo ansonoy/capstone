b3f98f8388e51463614d2c80c14ef8bb81031fa9
Anson <ansonoy00@gmail.com>
2023-11-30 17:08:21 -0700

    Updated pictures

--- capstone/app/shop/page.tsx
+++ capstone/app/shop/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import distillery from "@/public/distillery.png"
+import distillery from "@/public/product-page.png"
 import Heading from "../components/heading"
 import Products from "../components/products-card"
 import getProducts from "@/action/get-products"
